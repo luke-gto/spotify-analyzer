18e7f82a9f2c9ef797c23800539a035e25694e3f
luca-go <54832122+luca-go@users.noreply.github.com>
2022-04-29 10:03:16 +0200

    better formatting

--- src/spotify_analyzer.py
+++ src/spotify_analyzer.py
@@ -7,19 +7,22 @@ from PyQt5.QtWidgets import QMessageBox
 import os
 from datetime import datetime
 
-def get_time():
+
+def get_time():  ### for filenames
     now = datetime.now()
- 
+
     dt_string = now.strftime("%d-%m-%Y-%H:%M:%S")
 
     return dt_string
 
+
 def normal_round(num, ndigits=0):
-        if ndigits == 0:
-            return int(num + 0.5)
-        else:
-            digit_value = 10 ** ndigits
-            return int(num * digit_value + 0.5) / digit_value
+    if ndigits == 0:
+        return int(num + 0.5)
+    else:
+        digit_value = 10**ndigits
+        return int(num * digit_value + 0.5) / digit_value
+
 
 def last_played(songs_num):
     scope = "user-read-recently-played"
@@ -33,143 +36,226 @@ def last_played(songs_num):
     uri_list = []
     feature_list = []
 
-    for item in (results['items']):
-        title_list.append(item['track']['name'])
-        artist_list.append(item['track']['album']['artists'][0]['name'])
-        album_list.append(item['track']['album']['name'])
-        date_list.append(item['played_at'])
-        link_list.append(item['track']['external_urls']['spotify'])
-        uri_list.append(item['track']['uri'])
+    for item in results["items"]:
+        title_list.append(item["track"]["name"])
+        artist_list.append(item["track"]["album"]["artists"][0]["name"])
+        album_list.append(item["track"]["album"]["name"])
+        date_list.append(item["played_at"])
+        print(date_list)
+        link_list.append(item["track"]["external_urls"]["spotify"])
+        uri_list.append(item["track"]["uri"])
 
-    df = pd.DataFrame({'track_title':title_list, 'artist':artist_list, 'album':album_list, 'date': date_list, 'link': link_list, 'uri': uri_list})
+    df = pd.DataFrame(
+        {
+            "track_title": title_list,
+            "artist": artist_list,
+            "album": album_list,
+            "date": date_list,
+            "link": link_list,
+            "uri": uri_list,
+        }
+    )
 
     for item in uri_list:
 
         feature_list.append(track_feature(item))
 
-    df2 = pd.DataFrame(feature_list, columns=('danceability_%','energy_%', 'positivity_%', 'tempo_BPM', 'loudness_dB',
-            'speechiness_%', 'instrumentalness_%', 'duration_seconds'))
+    df2 = pd.DataFrame(
+        feature_list,
+        columns=(
+            "danceability_%",
+            "energy_%",
+            "positivity_%",
+            "tempo_BPM",
+            "loudness_dB",
+            "speechiness_%",
+            "instrumentalness_%",
+            "duration_seconds",
+        ),
+    )
 
     data_df = pd.concat([df, df2], axis=1)
 
-    av_dance = normal_round(data_df['danceability_%'].mean(axis=0), 2)
-    av_energy = normal_round(data_df['energy_%'].mean(axis=0), 2)
-    av_positivity = normal_round(data_df['positivity_%'].mean(axis=0), 2)
-    av_tempo = data_df['tempo_BPM'].mean(axis=0)
-    av_loudness = data_df['loudness_dB'].mean(axis=0)
-    av_speechiness = normal_round(data_df['speechiness_%'].mean(axis=0), 2)
-    av_instrument = normal_round(data_df['instrumentalness_%'].mean(axis=0), 2)
-    av_duration = data_df['duration_seconds'].mean(axis=0)
+    av_dance = normal_round(data_df["danceability_%"].mean(axis=0), 2)
+    av_energy = normal_round(data_df["energy_%"].mean(axis=0), 2)
+    av_positivity = normal_round(data_df["positivity_%"].mean(axis=0), 2)
+    av_tempo = data_df["tempo_BPM"].mean(axis=0)
+    av_loudness = data_df["loudness_dB"].mean(axis=0)
+    av_speechiness = normal_round(data_df["speechiness_%"].mean(axis=0), 2)
+    av_instrument = normal_round(data_df["instrumentalness_%"].mean(axis=0), 2)
+    av_duration = data_df["duration_seconds"].mean(axis=0)
 
-    av_df = pd.DataFrame({'average_danceability_%':av_dance, 'average_energy_%':av_energy, 'average_positivity_%':av_positivity, 
-        'average_tempo':av_tempo, 'average_loudness_dB':av_loudness, 'average_speechiness_%':av_speechiness, 'average_instrument_%':av_instrument,
-        'av_duration': av_duration}, index=[0])
-
+    av_df = pd.DataFrame(
+        {
+            "average_danceability_%": av_dance,
+            "average_energy_%": av_energy,
+            "average_positivity_%": av_positivity,
+            "average_tempo": av_tempo,
+            "average_loudness_dB": av_loudness,
+            "average_speechiness_%": av_speechiness,
+            "average_instrument_%": av_instrument,
+            "av_duration": av_duration,
+        },
+        index=[0],
+    )
 
     return data_df, av_df
 
 
 def top_artist(time_range, songs_num):
-    scope = 'user-top-read'
+    scope = "user-top-read"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    results = sp.current_user_top_artists(limit=songs_num, offset=0, time_range=time_range)
+    results = sp.current_user_top_artists(
+        limit=songs_num, offset=0, time_range=time_range
+    )
     name_list = []
     genre_list = []
     followers_list = []
     url_list = []
 
-    for item in (results['items']):
-        name_list.append(item['name'])
-        genre_list.append(item['genres'])
-        followers_list.append(item['followers']['total'])
-        url_list.append(item['external_urls']['spotify'])
+    for item in results["items"]:
+        name_list.append(item["name"])
+        genre_list.append(item["genres"])
+        followers_list.append(item["followers"]["total"])
+        url_list.append(item["external_urls"]["spotify"])
 
-    df = pd.DataFrame({'artist_name':name_list, 'genre':genre_list, 'followers':followers_list, 'URL': url_list})
+    df = pd.DataFrame(
+        {
+            "artist_name": name_list,
+            "genre": genre_list,
+            "followers": followers_list,
+            "URL": url_list,
+        }
+    )
 
     return df
 
+
 def tracks_analyzer(time_range, songs_num):
-    scope = 'user-top-read'
+    scope = "user-top-read"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    results = sp.current_user_top_tracks(limit=songs_num, offset=0, time_range=time_range)
+    results = sp.current_user_top_tracks(
+        limit=songs_num, offset=0, time_range=time_range
+    )
     title_list = []
     artist_list = []
     link_list = []
     url_list = []
     feature_list = []
 
-    for item in (results['items']):
-        title_list.append(item['name'])
-        artist_list.append(item['artists'][0]['name'])
-        url_list.append(item['external_urls']['spotify'])
-        link_list.append((item['uri'][14:]))
+    for item in results["items"]:
+        title_list.append(item["name"])
+        artist_list.append(item["artists"][0]["name"])
+        url_list.append(item["external_urls"]["spotify"])
+        link_list.append((item["uri"][14:]))
 
-    df = pd.DataFrame({'track_title':title_list, 'artist_name':artist_list, 'URL': url_list, 'track_id':link_list})
+    df = pd.DataFrame(
+        {
+            "track_title": title_list,
+            "artist_name": artist_list,
+            "URL": url_list,
+            "track_id": link_list,
+        }
+    )
 
     for item in link_list:
 
         feature_list.append(track_feature(item))
 
-    df2 = pd.DataFrame(feature_list, columns=('danceability_%','energy_%', 'positivity_%', 'tempo_BPM', 'loudness_dB',
-            'speechiness_%', 'instrumentalness_%', 'duration_seconds'))
+    df2 = pd.DataFrame(
+        feature_list,
+        columns=(
+            "danceability_%",
+            "energy_%",
+            "positivity_%",
+            "tempo_BPM",
+            "loudness_dB",
+            "speechiness_%",
+            "instrumentalness_%",
+            "duration_seconds",
+        ),
+    )
 
     data_df = pd.concat([df, df2], axis=1)
 
-    av_dance = normal_round(data_df['danceability_%'].mean(axis=0), 2)
-    av_energy = normal_round(data_df['energy_%'].mean(axis=0), 2)
-    av_positivity = normal_round(data_df['positivity_%'].mean(axis=0), 2)
-    av_tempo = data_df['tempo_BPM'].mean(axis=0)
-    av_loudness = data_df['loudness_dB'].mean(axis=0)
-    av_speechiness = normal_round(data_df['speechiness_%'].mean(axis=0), 2)
-    av_instrument = normal_round(data_df['instrumentalness_%'].mean(axis=0), 2)
-    av_duration = data_df['duration_seconds'].mean(axis=0)
+    av_dance = normal_round(data_df["danceability_%"].mean(axis=0), 2)
+    av_energy = normal_round(data_df["energy_%"].mean(axis=0), 2)
+    av_positivity = normal_round(data_df["positivity_%"].mean(axis=0), 2)
+    av_tempo = data_df["tempo_BPM"].mean(axis=0)
+    av_loudness = data_df["loudness_dB"].mean(axis=0)
+    av_speechiness = normal_round(data_df["speechiness_%"].mean(axis=0), 2)
+    av_instrument = normal_round(data_df["instrumentalness_%"].mean(axis=0), 2)
+    av_duration = data_df["duration_seconds"].mean(axis=0)
 
-    av_df = pd.DataFrame({'average_danceability_%':av_dance, 'average_energy_%':av_energy, 'average_positivity_%':av_positivity, 
-        'average_tempo':av_tempo, 'average_loudness_dB':av_loudness, 'average_speechiness_%':av_speechiness, 'average_instrument_%':av_instrument,
-        'av_duration': av_duration}, index=[0])
+    av_df = pd.DataFrame(
+        {
+            "average_danceability_%": av_dance,
+            "average_energy_%": av_energy,
+            "average_positivity_%": av_positivity,
+            "average_tempo": av_tempo,
+            "average_loudness_dB": av_loudness,
+            "average_speechiness_%": av_speechiness,
+            "average_instrument_%": av_instrument,
+            "av_duration": av_duration,
+        },
+        index=[0],
+    )
 
     return data_df, av_df
 
 
 def track_feature(link):
-
     def stats_percentage(value):
-        return normal_round((float(value)/1) * 100, 2)
+        return normal_round((float(value) / 1) * 100, 2)
 
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
     analysis = sp.audio_features(link)
-    danceability = stats_percentage((analysis[0]['danceability']))
-    energy = stats_percentage((analysis[0]['energy']))
-    positivity = stats_percentage((analysis[0]['valence']))
-    tempo = (analysis[0]['tempo'])
-    loudness = (analysis[0]['loudness'])
-    speechiness = stats_percentage((analysis[0]['speechiness']))
-    instrumentalness = stats_percentage((analysis[0]['instrumentalness']))
-    duration = (analysis[0]['duration_ms'])/1000.0
+    danceability = stats_percentage((analysis[0]["danceability"]))
+    energy = stats_percentage((analysis[0]["energy"]))
+    positivity = stats_percentage((analysis[0]["valence"]))
+    tempo = analysis[0]["tempo"]
+    loudness = analysis[0]["loudness"]
+    speechiness = stats_percentage((analysis[0]["speechiness"]))
+    instrumentalness = stats_percentage((analysis[0]["instrumentalness"]))
+    duration = (analysis[0]["duration_ms"]) / 1000.0
 
-    return danceability, energy, positivity, tempo, loudness, speechiness, instrumentalness, duration
+    return (
+        danceability,
+        energy,
+        positivity,
+        tempo,
+        loudness,
+        speechiness,
+        instrumentalness,
+        duration,
+    )
+
 
 def export_ods(dataframe, av_df, working_directory, file_name):
 
     os.chdir(working_directory)
 
-    with pd.ExcelWriter('spotify-' + file_name + '-' + str(get_time()) + '.xlsx', 
-                        engine='xlsxwriter', 
-                        options={'strings_to_urls': True}) as writer:
+    with pd.ExcelWriter(
+        "spotify-" + file_name + "-" + str(get_time()) + ".xlsx",
+        engine="xlsxwriter",
+        options={"strings_to_urls": True},
+    ) as writer:
 
-        dataframe.to_excel(writer, sheet_name='Top_tracks')
-        av_df.to_excel(writer, sheet_name='Average_values')
+        dataframe.to_excel(writer, sheet_name="Top_tracks")
+        av_df.to_excel(writer, sheet_name="Average_values")
 
     success_export(working_directory)
+
 
 def export_csv(dataframe, av_df, working_directory, file_name):
-    
+
     os.chdir(working_directory)
 
-    dataframe.to_csv('spotify-' + file_name + '-' + get_time() + '.csv')
-    av_df.to_csv('average_values_spotify_data.csv')
+    dataframe.to_csv("spotify-" + file_name + "-" + get_time() + ".csv")
+    av_df.to_csv("average_values_spotify_data.csv")
 
     success_export(working_directory)
+
 
 def success_export(directory):
     msg = QMessageBox()
--- src/table.py
+++ src/table.py
@@ -9,19 +9,22 @@ from pathlib import Path
 d = str(Path(__file__).resolve().parents[1])
 
 print(d)
+
+
 class average_window(QWidget):
-
     def __init__(self, dataframe):
-        self.data = dataframe        
+        self.data = dataframe
 
-    def createWindow(self,WindowWidth,WindowHeight):
-        
-        parent=None
-        super(average_window,self).__init__(parent)
+    def createWindow(self, WindowWidth, WindowHeight):
+
+        parent = None
+        super(average_window, self).__init__(parent)
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-        self.resize(WindowWidth,WindowHeight)
+        self.resize(WindowWidth, WindowHeight)
         self.tableWidget = QtWidgets.QTableWidget(self)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Ignored)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Ignored
+        )
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.tableWidget.sizePolicy().hasHeightForWidth())
@@ -30,7 +33,9 @@ class average_window(QWidget):
         self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
         self.tableWidget.setGeometry(QtCore.QRect(0, 0, 866, 745))
         self.tableWidget.setMouseTracking(True)
-        self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.tableWidget.setSizeAdjustPolicy(
+            QtWidgets.QAbstractScrollArea.AdjustToContents
+        )
         self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.tableWidget.setTabKeyNavigation(False)
         self.tableWidget.setProperty("showDropIndicator", False)
@@ -45,14 +50,14 @@ class average_window(QWidget):
             font = QtGui.QFont()
             font.setPointSize(12)
             font.setBold(True)
-            font.setWeight(75)            
-            horizontalHeader.setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeToContents)
+            font.setWeight(75)
+            horizontalHeader.setSectionResizeMode(
+                i, QtWidgets.QHeaderView.ResizeToContents
+            )
             horizontalHeader.setFont(font)
 
 
-
 class Ui_tableWindow(object):
-
     def __init__(self, dataframe, av_df):
         self.data = dataframe
         self.av_data = av_df
@@ -61,10 +66,18 @@ class Ui_tableWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(866, 702)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(d + "/resources/images/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        icon.addPixmap(
+            QtGui.QPixmap(d + "/resources/images/icon.png"),
+            QtGui.QIcon.Normal,
+            QtGui.QIcon.On,
+        )
         MainWindow.setWindowIcon(icon)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(d + "/resources/images/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        icon.addPixmap(
+            QtGui.QPixmap(d + "/resources/images/icon.png"),
+            QtGui.QIcon.Normal,
+            QtGui.QIcon.On,
+        )
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
@@ -72,7 +85,9 @@ class Ui_tableWindow(object):
         self.verticalLayout.setObjectName("verticalLayout")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
         horizontalHeader = self.tableWidget.horizontalHeader()
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Ignored)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Ignored
+        )
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.tableWidget.sizePolicy().hasHeightForWidth())
@@ -80,7 +95,9 @@ class Ui_tableWindow(object):
         self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
         self.tableWidget.setGeometry(QtCore.QRect(0, 0, 866, 745))
         self.tableWidget.setMouseTracking(True)
-        self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.tableWidget.setSizeAdjustPolicy(
+            QtWidgets.QAbstractScrollArea.AdjustToContents
+        )
         self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.tableWidget.setTabKeyNavigation(False)
         self.tableWidget.setProperty("showDropIndicator", False)
@@ -95,24 +112,25 @@ class Ui_tableWindow(object):
         self.menubar.setGeometry(QtCore.QRect(0, 0, 866, 200))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
-        
+
         if not self.av_data.empty:
 
-            self.menuStats = QtWidgets.QMenu(self.menubar)            
+            self.menuStats = QtWidgets.QMenu(self.menubar)
             self.menuStats.setObjectName("menuStats")
             self.actionStats = QtWidgets.QAction(MainWindow)
             self.actionStats.setObjectName("actionStats")
             self.menubar.addAction(self.actionStats)
             self.retranslateUi(MainWindow)
-            self.actionStats.triggered.connect((lambda: self.open_average_window(self.av_data)))
-
+            self.actionStats.triggered.connect(
+                (lambda: self.open_average_window(self.av_data))
+            )
 
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.load_data()
-        
+
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -120,24 +138,25 @@ class Ui_tableWindow(object):
             font = QtGui.QFont()
             font.setPointSize(12)
             font.setBold(True)
-            font.setWeight(75)            
-            horizontalHeader.setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeToContents)
+            font.setWeight(75)
+            horizontalHeader.setSectionResizeMode(
+                i, QtWidgets.QHeaderView.ResizeToContents
+            )
             horizontalHeader.setFont(font)
 
         self.tableWidget.itemDoubleClicked.connect(self.OpenLink)
 
     def open_average_window(self, dataframe):
-        self.mySubwindow=average_window(dataframe)
-        self.mySubwindow.createWindow(500,400)
+        self.mySubwindow = average_window(dataframe)
+        self.mySubwindow.createWindow(500, 400)
         self.mySubwindow.show()
-
 
     def OpenLink(self):
         for index in self.tableWidget.selectedIndexes():
             value = str(self.data.iloc[index.row()][index.column()])
             if value.startswith("http://") or value.startswith("https://"):
                 webbrowser.open(value)
-    
+
     def load_data(self):
         row = 0
         col = 0
@@ -146,10 +165,12 @@ class Ui_tableWindow(object):
             list.append(col_name)
             self.tableWidget.setHorizontalHeaderLabels(list)
 
-        for row in range (0, self.data.shape[0]):
-                for col in range (0, self.data.shape[1]):
-                        data = str(self.data.iloc[row, col] )
-                        self.tableWidget.setItem(row, col, QtWidgets.QTableWidgetItem(str(data)))
+        for row in range(0, self.data.shape[0]):
+            for col in range(0, self.data.shape[1]):
+                data = str(self.data.iloc[row, col])
+                self.tableWidget.setItem(
+                    row, col, QtWidgets.QTableWidgetItem(str(data))
+                )
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -158,8 +179,10 @@ class Ui_tableWindow(object):
             self.menuStats.setTitle(_translate("MainWindow", "Average values"))
             self.actionStats.setText(_translate("MainWindow", "Average values"))
 
+
 def show_table(dataframe, av_df):
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_tableWindow(dataframe, av_df)
